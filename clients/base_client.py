4e3800a2350950431f6e66140a707b54b8d189b0
Tutorial <foditoon1@gmail.com>
2026-04-23 18:35:07 +0400

    Создан базовый клиент для работы с API

---
+++ clients/base_client.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, Optional
+import requests
+import logging
+
+from config import BASE_URL, TIMEOUT_SECONDS, HEADERS
+
+logger = logging.getLogger(__name__)
+
+class BaseClient:
+    """
+    Базовый клиент для взаимодействия с API.
+    Содержит общие методы для отправки HTTP-запросов и обработки ответов.
+    """
+
+    def __init__(
+        self,
+        base_url: str = BASE_URL,
+        timeout: int = TIMEOUT_SECONDS
+    ) -> None:
+        self.base_url = base_url
+        self.timeout = timeout
+        self.session = requests.Session()
+        self.session.headers.update(HEADERS)
+
+    def send_request(
+        self,
+        method: str,
+        path: str,
+        json: Optional[Dict[str, Any]] = None,
+    ) -> requests.Response:
+        """Отправка запроса и принятие ответа"""
+        url = f"{self.base_url}{path}"
+        logger.info(f"Отправляем {method} запрос на URL: {url}{path} с данными: {json}")
+        try:
+            return self.session.request(
+                method=method,
+                url=url,
+                json=json,
+                timeout=self.timeout_seconds,
+            )
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Ошибка при отправке запроса: {e}")
+            raise
---
+++ config.py
@@ -0,0 +1,13 @@
+BASE_URL = "http://localhost:8080"
+CREATE_URL = "api/create"
+DELETE_URL = "api/delete/"
+GET_ALL_URL = "api/getAll"
+GET_BY_ID_URL = "api/get/"
+PATCH_URL = "api/patch/"
+
+TIMEOUT_SECONDS = 5
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
